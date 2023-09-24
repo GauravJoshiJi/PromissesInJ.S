857d3b0796b7cf07f36c0a6b20316c792e0a4682
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-09-24 23:45:13 +0530

    First Commit for Promisses topic

---
+++ script.js
@@ -0,0 +1,63 @@
+//------------------------------------------
+//1>Basic of Promiss
+
+// function delayedFx() {
+//   setTimeout(function () {
+//     console.log("Hello World");
+//   }, 3000);
+// }
+// delayedFx();
+
+//---------------------------------------------
+
+//2> Promiss Syntax
+
+// console.log("Start");
+
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //resolve("1");
+//     reject();
+//   }, 2000);
+// });
+
+// p1.then((data) => {
+//   console.log(data);
+// }).catch(() => {
+//   console.log("Error");
+// });
+
+// console.log("End");
+//------------------------------------------
+
+//3> Promiss Chaning :-
+
+// console.log("Start");
+
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("1");
+//   }, 2000);
+// });
+
+// p1.then((data) => {
+//   console.log("then1", data);
+//   return data;
+// }).then((data) => {
+//   console.log("then2", data);
+// });
+
+// console.log("End");
+//--------------------------------------------
+
+//4> Fetch API :-
+
+//case1:-
+// fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
+//   console.log(response)
+// );
+
+//case2:-
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));

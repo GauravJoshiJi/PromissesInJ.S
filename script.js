2667a7e0a766babf64e26b71cd1dac5942674673
GauravJoshiJi <125949334+GauravJoshiJi@users.noreply.github.com>
2023-09-25 11:21:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 //------------------------------------------
 //1>Basic of Promiss
 
-// function delayedFx() {
-//   setTimeout(function () {
-//     console.log("Hello World");
-//   }, 3000);
-// }
-// delayedFx();
+function delayedFx() {
+  setTimeout(function () {
+    console.log("Hello World");
+  }, 3000);
+}
+delayedFx();
 
 //---------------------------------------------
 
 //2> Promiss Syntax
 
-// console.log("Start");
+console.log("Start");
 
-// const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     //resolve("1");
-//     reject();
-//   }, 2000);
-// });
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    //resolve("1");
+    reject();
+  }, 2000);
+});
 
-// p1.then((data) => {
-//   console.log(data);
-// }).catch(() => {
-//   console.log("Error");
-// });
+p1.then((data) => {
+  console.log(data);
+}).catch(() => {
+  console.log("Error");
+});
 
-// console.log("End");
+console.log("End");
 //------------------------------------------
 
 //3> Promiss Chaning :-
 
-// console.log("Start");
+console.log("Start");
 
-// const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("1");
-//   }, 2000);
-// });
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("1");
+  }, 2000);
+});
 
-// p1.then((data) => {
-//   console.log("then1", data);
-//   return data;
-// }).then((data) => {
-//   console.log("then2", data);
-// });
+p1.then((data) => {
+  console.log("then1", data);
+  return data;
+}).then((data) => {
+  console.log("then2", data);
+});
 
-// console.log("End");
+console.log("End");
 //--------------------------------------------
 
 //4> Fetch API :-
 
 //case1:-
-// fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
-//   console.log(response)
-// );
+fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
+  console.log(response)
+);
 
 //case2:-
 // fetch("https://jsonplaceholder.typicode.com/posts")

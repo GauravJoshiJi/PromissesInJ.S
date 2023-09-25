ed601acd67e5d38b76b5b741f30021209feb2c6a
GauravJoshiJi <125949334+GauravJoshiJi@users.noreply.github.com>
2023-09-25 11:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,6 @@ fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
 );
 
 //case2:-
-// fetch("https://jsonplaceholder.typicode.com/posts")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
